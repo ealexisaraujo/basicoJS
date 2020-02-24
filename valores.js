345b3ae876ca51af868f9876f4da59b41435b289
Alexis Araujo <ala1289@gmail.com>
2020-02-24 11:49:59 -0600

    Variables y elementos de las funciones

---
+++ valores.js
@@ -0,0 +1,23 @@
+//Valores primitivos (basicos que utilizamos para generar codigo)
+//Valores numericos
+40;
+typeof 40;
+
+//Cadenas de texto
+('Alexis');
+typeof 'Oscar';
+//Booleanos
+true;
+false;
+typeof true;
+//Tipo de valores vacios
+null;
+undefined;
+
+//Valores no primitivos
+[1, 3, 4];
+
+//Valores tipo de objetos
+{
+  nombre: 'Alexis';
+}
---
+++ variables.js
@@ -0,0 +1,9 @@
+// Variables es la representacion de algun lugar de la memoria
+// palabra reservada var
+var nombre = 'Alexis';
+
+//declarar
+var edad;
+
+//inicializar
+edad = 30;
