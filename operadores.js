1767bde931cc74ab28ce684fa4a45b038a783255
Alexis Araujo <ala1289@gmail.com>
2020-02-24 18:19:36 -0600

    Operadores: Asignacion, Comparacion y Aritmeticos

---
+++ operadores.js
@@ -0,0 +1,35 @@
+// Operadores binarios
+3 * 2;
+50 - 10;
+10 * 20;
+20 / 2;
+
+//Concatenacion de strings
+'Alexis' + 'Araujo';
+
+//Operadores unitarios
+//negacion de la negacion = true
+!false;
+
+//Operadores de asigancion
+//Asigamos un valor a una variable
+var a = 1;
+
+//Operadores para comparar
+//Compara los valores y devuelve "true" en este caso
+3 == 3;
+
+3 === '3'; //Compara y valida los tipos y valores. Devuelve "falso" en este caso
+
+5 < 3; //Compara y valida si el 5 es menor a 3
+5 > 3; //Compara y valida si el 5 es mayor a 3
+5 <= 6; //Compara y valida si el 5 es menor o igual al 6
+5 >= 6; //Compara y valida si el 5 es mayor o igual al 6
+
+a && b; //Valida si ambas variables son verdaderas para que se cumpla la condición
+a || b; //Aquí se cumple la condición si alguna de las dos variables es verdadera
+
+var edad = 40;
+edad++; //Incrementa el valor en 1
+
+edad += 2; //Incrementa el valor por 2
