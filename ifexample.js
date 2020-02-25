d5120d5c9cf07139a69e5a74fba8e4b41c2b0421
Alexis Araujo <ala1289@gmail.com>
2020-02-24 22:29:41 -0600

    Operadores ternarios

--- ifexample.js
+++ ifexample.js
@@ -1,4 +1,4 @@
-var edad = 18;
+var edad = 10;
 
 if (edad === 18) {
   console.log('Puedes votar, sera tu 1ra votacion');
@@ -7,3 +7,12 @@ if (edad === 18) {
 } else {
   console.log('Aun no puedes votar');
 }
+
+var resultado =
+  edad === 18
+    ? 'Puedes votar, sera tu 1ra votacion'
+    : edad > 18
+    ? 'Puedes votar de nuevo'
+    : 'Aun no puedes votar';
+
+console.log(resultado);
